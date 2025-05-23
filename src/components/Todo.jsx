da52d170eb36f5754db320b814e8077d5e8c0ff7
Hannah Waruguru <hannahwaruguru@Hannahs-MacBook-Pro.local>
2025-05-23 10:51:18 +0300

    add todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,3 +1,24 @@
-export default function Todo(){
-return <div>Todo</div>
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos,todo]);
+    setTodo("")
+  }
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          value={todo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map(item=><h3>{item}</h3>)}
+    </div>
+  );
 }
