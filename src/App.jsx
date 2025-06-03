a460592f9d200e19632bd49e3c88d865fb6a6b5e
Hannah Waruguru <hannahwaruguru@Hannahs-MacBook-Pro.local>
2025-06-03 11:24:07 +0300

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import InlineComponent from "./components/InlineComponent";
-import OutlineComponent from "./components/OutlineComponent";
 import Todo from "./components/Todo";
-
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        {/* <Todo /> */}
-        <InlineComponent />
-        <OutlineComponent />
+        <Header />
+        <Todo />
       </div>
     </>
   );
