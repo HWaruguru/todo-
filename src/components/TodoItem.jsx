fa2ef071b34f93d1313e4742725bf87fd9f98ad1
Hannah Waruguru <hannahwaruguru@Hannahs-MacBook-Pro.local>
2025-06-03 16:41:47 +0300

    add delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,21 @@
 import styles from "./todoitem.module.css";
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos}) {
+  function handleDelete(item){
+    console.log("Delete", item)
+    setTodos(todos.filter((todo)=>
+      todo !== item
+    ))
+  }
   return (
     <div className={styles.item}>
-      <div className={styles.itemname}>{item}</div>
-      <hr className={styles.line}/>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button onClick={()=>handleDelete(item)}className={styles.deleteButton}>x</button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css"
-export default function TodoList({todos}){
-    return <div className={styles.list}>{todos.map(item=><TodoItem key={item} item={item}/>)}</div>
+export default function TodoList({todos, setTodos}){
+    return <div className={styles.list}>{todos.map(item=><TodoItem key={item} item={item} todos={todos} setTodos={setTodos}/>)}</div>
 }
