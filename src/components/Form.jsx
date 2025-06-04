a74c7c477a88c0f4aa1ac2700b5e7af0726cf760
Hannah Waruguru <hannahwaruguru@Hannahs-MacBook-Pro.local>
2025-06-04 16:18:58 +0300

    add complete strikethrough and count total

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import styles from "./form.module.css";
 export default function Form({ todos, setTodos }) {
-  const [todo, setTodo] = useState("");
-
+  // const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({name: "", done: false});
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
-    setTodo("");
+    setTodo({name: "", done: false});
   }
   return (
     <form className={styles.todoform} action="" onSubmit={handleSubmit}>
       <div className={styles.inputContainer}>
         <input
           className={styles.modernIput}
-          onChange={(e) => setTodo(e.target.value)}
+          onChange={(e) => setTodo({name: e.target.value,done: false })}
           type="text"
-          value={todo}
+          value={todo.name}
           placeholder="Enter todo item..."
         />
         <button className={styles.modernButton} type="submit">
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-
+import Footer from "./Footer"
 import TodoList from "./TodoList";
 import Form from "./Form";
 export default function Todo() {
   const [todos, setTodos] = useState([]);
-
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos = todos.length
   return (
     <div>
-      <Form todos={todos} setTodos={setTodos}/>
+      <Form todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
+      <Footer completedTodos={completedTodos} totalTodos= {totalTodos}/>
     </div>
   );
 }
