c3dce1b92b113229e3c4d5a8bc1366195800ee26
Hannah Waruguru <hannahwaruguru@Hannahs-MacBook-Pro.local>
2025-06-04 16:40:08 +0300

    sort the todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css"
 export default function TodoList({todos, setTodos}){
-    return <div className={styles.list}>{todos.map(item=><TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos}/>)}</div>
+    const sortedTodos = todos.slice().sort((a,b)=>Number(a.done)-Number(b.done))
+    return <div className={styles.list}>{sortedTodos.map(item=><TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos}/>)}</div>
 }
